b2c113fb938a25188edf00fa426316f0c6fbab89
Matias Korhonen <me@matiaskorhonen.fi>
2026-07-03 15:34:32 +0300

    Pin tree-sitter-objc to a specific commit on master

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         .package(url: "https://github.com/fwcd/tree-sitter-kotlin", revision: "c8ac3d2627240160b999a2c100de3babbdb8f419"), // main
         .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-lua", exact: "0.3.0"),
         .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-markdown", exact: "0.5.3"),
-        .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-objc", branch: "master"),
+        .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-objc", revision: "181a81b8f23a2d593e7ab4259981f50122909fda"), // master
         .package(url: "https://github.com/tree-sitter/tree-sitter-ocaml.git", exact: "0.24.2"),
         .package(url: "https://github.com/tree-sitter-perl/tree-sitter-perl.git", branch: "release"),
         .package(url: "https://github.com/tree-sitter/tree-sitter-php.git", exact: "0.24.2"),
