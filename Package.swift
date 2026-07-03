d39347657b1cf1b2c2e52698238449f6cb7de827
Matias Korhonen <me@matiaskorhonen.fi>
2026-07-03 15:37:11 +0300

    Pin tree-sitter-rust to a specific commit on master

--- Package.swift
+++ Package.swift
@@ -46,7 +46,7 @@ let package = Package(
         .package(url: "https://github.com/tree-sitter/tree-sitter-python.git", exact: "0.23.6"),
         .package(url: "https://github.com/tree-sitter/tree-sitter-regex.git", exact: "0.25.0"),
         .package(url: "https://github.com/tree-sitter/tree-sitter-ruby.git", revision: "ad907a69da0c8a4f7a943a7fe012712208da6dee"), // master
-        .package(url: "https://github.com/tree-sitter/tree-sitter-rust.git", branch: "master"),
+        .package(url: "https://github.com/tree-sitter/tree-sitter-rust.git", revision: "77a3747266f4d621d0757825e6b11edcbf991ca5"), // master
         .package(url: "https://github.com/tree-sitter/tree-sitter-scala.git", branch: "master"),
         .package(url: "https://github.com/DerekStride/tree-sitter-sql.git", branch: "gh-pages"),
         .package(url: "https://github.com/alex-pinkus/tree-sitter-swift.git", branch: "with-generated-files"),
