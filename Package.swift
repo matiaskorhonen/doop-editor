ee1b2fa4702ab355da5fa51d30833809c717e231
Matias Korhonen <me@matiaskorhonen.fi>
2026-07-03 15:29:46 +0300

    Pin tree-sitter-generic to a specific commit on main

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/tree-sitter/tree-sitter-css.git", exact: "0.23.2"),
         .package(url: "https://github.com/camdencheek/tree-sitter-dockerfile.git", revision: "971acdd908568b4531b0ba28a445bf0bb720aba5"), // main
         .package(url: "https://github.com/elixir-lang/tree-sitter-elixir.git", revision: "c4f9f5a15ddad8635ba59a5b99c2e9124e74ad91"), // main
-        .package(url: "https://github.com/matiaskorhonen/tree-sitter-generic.git", branch: "main"),
+        .package(url: "https://github.com/matiaskorhonen/tree-sitter-generic.git", revision: "4a967c7ca3ce6116827ddf090eaa4ef7d10c03df"), // main
         .package(url: "https://github.com/tree-sitter/tree-sitter-go.git", exact: "0.25.0"),
         .package(url: "https://github.com/camdencheek/tree-sitter-go-mod.git", branch: "main"),
         .package(url: "https://github.com/tree-sitter/tree-sitter-haskell.git", branch: "master"),
