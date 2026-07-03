a772fa20e377e1ce64c0751ff54314a2d3302af9
Matias Korhonen <me@matiaskorhonen.fi>
2026-07-03 15:32:49 +0300

    Pin tree-sitter-java to a specific commit on master

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .package(url: "https://github.com/camdencheek/tree-sitter-go-mod.git", revision: "2e886870578eeba1927a2dc4bd2e2b3f598c5f9a"), // main
         .package(url: "https://github.com/tree-sitter/tree-sitter-haskell.git", revision: "0975ef72fc3c47b530309ca93937d7d143523628"), // master
         .package(url: "https://github.com/tree-sitter/tree-sitter-html.git", revision: "73a3947324f6efddf9e17c0ea58d454843590cc0"), // master
-        .package(url: "https://github.com/tree-sitter/tree-sitter-java.git", branch: "master"),
+        .package(url: "https://github.com/tree-sitter/tree-sitter-java.git", revision: "e10607b45ff745f5f876bfa3e94fbcc6b44bdc11"), // master
         .package(url: "https://github.com/tree-sitter/tree-sitter-javascript.git", exact: "0.23.1"),
         .package(url: "https://github.com/tree-sitter/tree-sitter-jsdoc.git", exact: "0.23.2"),
         .package(url: "https://github.com/tree-sitter/tree-sitter-json.git", exact: "0.24.8"),
